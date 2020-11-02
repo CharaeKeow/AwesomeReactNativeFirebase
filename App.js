5fba1d4d4bc63fd7f8a2a98fc1e0a3a01b6fa8e8
CharaeKeow <charaekeow11@gmail.com>
2020-11-03 02:03:20 +0800

    complete code as per tutorial. however not tested yet due to my internet prob

--- App.js
+++ App.js
@@ -1,21 +1,99 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler'
+import React, { useEffect, useState } from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { LoginScreen, HomeScreen, RegistrationScreen } from './src/screens'
+import { decode, encode } from 'base-64'
+if (!global.btoa) { global.btoa = encode }
+if (!global.atob) { global.atob = decode }
+import { Platform, InteractionManager } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const _setTimeout = global.setTimeout;
+const _clearTimeout = global.clearTimeout;
+const MAX_TIMER_DURATION_MS = 60 * 1000;
+if (Platform.OS === 'android') {
+  // Work around issue `Setting a timer for long time`
+  // see: https://github.com/firebase/firebase-js-sdk/issues/97
+  const timerFix = {};
+  const runTask = (id, fn, ttl, args) => {
+    const waitingTime = ttl - Date.now();
+    if (waitingTime <= 1) {
+      InteractionManager.runAfterInteractions(() => {
+        if (!timerFix[id]) {
+          return;
+        }
+        delete timerFix[id];
+        fn(...args);
+      });
+      return;
+    }
+
+    const afterTime = Math.min(waitingTime, MAX_TIMER_DURATION_MS);
+    timerFix[id] = _setTimeout(() => runTask(id, fn, ttl, args), afterTime);
+  };
+
+  global.setTimeout = (fn, time, ...args) => {
+    if (MAX_TIMER_DURATION_MS < time) {
+      const ttl = Date.now() + time;
+      const id = '_lt_' + Object.keys(timerFix).length;
+      runTask(id, fn, ttl, args);
+      return id;
+    }
+    return _setTimeout(fn, time, ...args);
+  };
+
+  global.clearTimeout = id => {
+    if (typeof id === 'string' && id.startsWith('_lt_')) {
+      _clearTimeout(timerFix[id]);
+      delete timerFix[id];
+      return;
+    }
+    _clearTimeout(id);
+  };
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createStackNavigator();
+
+export default function App() {
+  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(null)
+
+  if (loading) {
+    return (
+      <></>
+    )
+  }
+
+  useEffect(() => {
+    const usersRef = firebase.firestore().collection('users');
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        usersRef
+          .doc(user.uid)
+          .get()
+          .then((document) => {
+            const userData = document.data()
+            setLoading(false)
+            setUser(userData)
+          })
+          .catch((error) => {
+            setLoading(false)
+          });
+      } else {
+        setLoading(false)
+      }
+    });
+  }, []);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName={user ? 'Home' : 'Login'}>
+        <Stack.Screen name="Home">
+          {props => <HomeScreen {...props} extraData={user} />}
+        </Stack.Screen>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Registration" component={RegistrationScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
